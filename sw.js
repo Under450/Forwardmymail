144c35bc9557b44561dfa68eea96f768f617321c
Under450 <caj@me.com>
2026-03-03 20:10:12 +0000

    Cache bust v1772568611

--- sw.js
+++ sw.js
@@ -1,30 +1,4 @@
-const CACHE = 'fmm-v1772567782';
-const FILES = ['/fmm-app.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('fmm-app.html')) {
-    e.respondWith(
-      fetch(e.request, { cache: 'no-store' })
-        .then(resp => {
-          const clone = resp.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-          return resp;
-        })
-        .catch(() => caches.match(e.request))
-    );
-  }
-});
+const CACHE = 'fmm-v1772568611';
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/fmm-app.html']))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { if (e.request.mode === 'navigate') { e.respondWith(fetch(e.request).catch(() => caches.match('/fmm-app.html'))); return; } e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
