2ab41451dcc47b4527f93230a8d93fa6696476d9
Under450 <caj@me.com>
2026-03-04 17:13:01 +0000

    Cache bust for revert

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fmm-v1772641614';
+const CACHE = 'fmm-v1772644380';
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/fmm-app.html']))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => { if (e.request.mode === 'navigate') { e.respondWith(fetch(e.request).catch(() => caches.match('/fmm-app.html'))); return; } e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
